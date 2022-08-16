5e6aa30174dcced30dc90942b9fa1e2332f57b50
Justin <ju3tin@hotmail.co.uk>
2022-08-16 16:19:37 +0100

    make it better

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post("/test12a", function (req,res){
   
   let mailOptions = {
     from: 'ju3tin95@gmail.com',
-    to: 'ju3in@hotmail.co.uk',
+    to: 'ju3tin@hotmail.co.uk',
     subject: `The subject goes here`,
     html: `The body of the email goes here in HTML`,
   };
