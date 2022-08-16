017560858174f5d35154d2760d3540f45c24070c
Justin <ju3tin@hotmail.co.uk>
2022-08-16 16:24:48 +0100

    make it better

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.post("/test12a", function (req,res){
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'ju3tin95',
+      user: 'ju3tin95@gmail.com',
       pass: 'Grierson1979.',
     },
   });
