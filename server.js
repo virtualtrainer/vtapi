58106f3eb007bf373174df283a2ca3013f4f6f15
Justin <ju3tin@hotmail.co.uk>
2022-08-17 23:09:20 +0100

    make it better

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post("/test12a", function (req,res){
   
   let mailOptions = {
     from: 'ju3tin95@gmail.com',
-    to: req.body.to,
+    to: 'ju3tin@hotmail.co.uk',
     subject: `The subject goes here`,
     html: `The body of the email goes here in HTML`,
   };
